2b80d3333382abe8666b735227adb0ae2ec9e3b5
MARIUS SERTVYTIS <marius.sert@gmail.com>
2023-02-23 20:49:29 +0200

    calculator

---
+++ calc.py
@@ -0,0 +1,5 @@
+def add(x: int, y: int) -> int:
+    return x + ydef sub(x: int, y: int) -> int:
+    return x - ydef prod(x: int, y: int) -> int:
+    return x * ydef div(x: int, y: int) -> int:
+    return x // y
